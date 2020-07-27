935b9323459c2f049f1a99c396bf966cc0dbd52d
Yago <yagomichalak@gmail.com>
2020-07-27 12:12:31 -0300

    bot

--- cogs/games.py
+++ cogs/games.py
@@ -208,14 +208,14 @@ class Games(commands.Cog):
 		# Checks if the bot is in a voice channel
 		if not voice_client:
 			await voice.channel.connect()
-			await asyncio.sleep(2)
+			await asyncio.sleep(1)
 			voice_client: discord.VoiceClient = discord.utils.get(self.client.voice_clients, guild=member.guild)
 
 		# Checks if the bot is in the same voice channel that the user
 		if voice.channel == voice_client.channel:
 			# get a random language audio
 			path, language, audio = self.get_random_language()
-
+			await asyncio.sleep(1)
 			# Plays the song
 			if not voice_client.is_playing():
 				audio_source = discord.FFmpegPCMAudio(path)
