79bced9f9e704e9c97add28a3244558763742ad8
Yago <yagomichalak@gmail.com>
2020-12-07 12:34:14 -0300

    adding more questions'

--- extra/xmas.py
+++ extra/xmas.py
@@ -27,227 +27,153 @@ items: Dict[str, Dict[str, List[Dict[str, str]]]] = {
 questions: Dict[str, List[Dict[str, str]]] = {
 	"all": [
 		{"question": "What is the present participle form of hunt?", "answer": """Hunting"""},
-
 		{"question": """What is the meaning of the Latin root corpus?""", "answer": """Body"""},
-
 		{"question": """The work of what famous American poet inspired the musical "Cats"?""", "answer": """T.S. Eliot"""},
-
 		{"question": """What is a 12 letter word meaning "having the ability of using both hands with equal skill or ease"?""", "answer": """Ambidextrous"""},
-
 		{"question": """Which of these is a hyperbole, The Horse Is Eating or I Could Eat A Horse?""", "answer": """I Could Eat A Horse"""},
-
 		{"question": """What literary term means a word that has a similar meaning to another word?""", "answer": """Synonym"""},
-
 		{"question": """What word in English means prior to the Civil War?""", "answer": """Antebellum"""},
-
 		{"question": """What is the meaning of the greek prefix poly?""", "answer": """Many"""},
-
 		{"question": """Who said "it is far safer to be feared than loved"?""", "answer": """Niccolo Machiavelli"""},
-
 		{"question": """What is the meaning of the greek prefix geo?""", "answer": """Earth"""},
-
 		{"question": """What is an eight-letter word meaning "one who advocates or practices altruism"?""", "answer": """Altruist"""},
-
 		{"question": """What part of speech is "susceptible", meaning "easily under a specified power or influence"?""", "answer": """Adjective"""},
-
 		{"question": """What is a 6 letter word meaning "a way of approach or entrance; passage"?""", "answer": """Access"""},
-
 		{"question": """What is a 10 letter word meaning "to mix or blend together in a homogeneous body"?""", "answer": """Amalgamate"""},
-
 		{"question": """What is the singular possessive form of "boy"?""", "answer": """Boy's"""},
-
 		{"question": """What is the meaning of the Latin root ultra?""", "answer": """Beyond"""},
-
 		{"question": """In "Mary Poppins", the Banks family lived on what street?""", "answer": """Cherry Tree Lane"""},
-
 		{"question": """What is an 8 letter word meaning "to give credit or authority to"?""", "answer": """Accredit"""},
-
 		{"question": """What punctuation mark ends an imperative sentence?""", "answer": """!"""},
-
 		{"question": """Which word would define the Latin root lingua?""", "answer": """Language"""},
-
 		{"question": """What does "minuscule" mean in latin?""", "answer": """Smaller"""},
-
 		{"question": """What is the meaning of the Latin root sol?""", "answer": """Sun"""},
-
 		{"question": """Cerveza means beer in which language?""", "answer": """Spanish"""},
-
 		{"question": """“Same same but different” is a common phrase meaning “similar” in which country?""", "answer": """Thailand"""},
-
 		{"question": """You spill a drink on someone at a bar in Rome. You apologise and they reply: “Ti rifaccio la dentatura.” Should you be concerned?""", "answer": """yes"""},
-
 		{"question": """When would you say namaste to someone?""", "answer": """Greeting"""},
-
 		{"question": """Hakuna matata (meaning no worries) was a phrase made popular around the world by the Lion King, but which language is it?""", "answer": """Swahili"""},
-
 		{"question": """In which country do locals use the phrase “not my circus, not my monkeys” when they mean “not my problem”?""", "answer": """Poland"""},
-
 		{"question": """From what language does the english word "Tornado" stems from?""", "answer": """Spanish"""},
-
 		{"question": """From which language does the word Lemming originate? """, "answer": """Norwegian"""},
-
 		{"question": """Postage stamps from Switzerland bear the inscription 'Helvetia'. In which language does Helvetia mean "Switzerland"? """, "answer": """latin"""},
-
 		{"question": """What language is "Stern" published in?""" , "answer": """German"""},
-
 		{"question": """Which language is Afrikaans derived from? """, "answer": """Dutch"""},
-		
 		{"question": """In which country is the language Sinhalese spoken? """, "answer": """Sri Lanka"""},
-
 		{"question": """In Morse code, how is the letter 'S' represented? """, "answer": """..."""},
-
 		{"question": """What does the Australian slang word 'dinkum' mean? """, "answer": """Honest"""},
-
 		{"question": """Der Spiegel is a famous German publication, when translated into English it shares its name with a famous British publication, which? """, "answer": """The Mirror"""},
-
 		{"question": """Which European language is an official language of Angola? """, "answer": """Portuguese"""},
-
 		{"question": """What is the official language of Pakistan? """, "answer": """Urdu"""},
-
 		{"question": """What is Finland's name in its own language? """, "answer": """Suomi """},
-
 		{"question": """What is the dominant classical language of the Indian subcontinent? """, "answer": """Sanskrit"""},
-
 		{"question": """Which is the only French-speaking republic in the Americas? """, "answer": """Haiti"""},
-
 		{"question": """What is the Italian word for the Virgin Mary which when translated literally means 'my lady'? """, "answer": """Madonna"""},
-
 		{"question": """Which African city's name, when literally translated, means 'White House'? """, "answer": """Casablanca"""},
-
 		{"question": """Which cities name when translated from Spanish means 'The Meadows'?""", "answer": """Las Vegas"""},
-
 		{"question": """A sound made by an 'Anatidae' and a pejorative name of a fraudulent or ignorant pretender of a certain profession. Five letters, second letter "U". """, "answer": """Quack"""},
-
 		{"question": """From what language does the english word "Ombudsman " stems from?""", "answer": """Swedish """},
-
 		{"question": """In Japan, what is a gaijin? """, "answer": """foreigner"""},
-
 		{"question": """Which medical emergency term stems from the French verb meaning 'to separate'? """, "answer": """Triage"""},
-
 		{"question": """What is the ancient Persian word for nation or land? """, "answer": """Stan"""},
-
 		{"question": """Which word for a meeting or conference stems from the Greek word for a drinking party? """, "answer": """Symposium"""},
-
 		{"question": """The English language word 'brainwashing' was first coined during which war? """, "answer": """The Korean War"""},
-
 		{"question": """The name of which central European capital city means 'ford'? (as in river ford)""", "answer": """Prague"""},
-
 		{"question": """Which well-known word stems from the Latin for 'knowledge'?""", "answer": """Science"""},
-
 		{"question": """Turks Head, Flemish Flake, Dutch Cringle and Granny are all examples of what?""", "answer": """Knots"""},
-
 		{"question": """The logogram '&', known as the Ampersand, is a contraction of which words?""", "answer": """and per se, and"""},
-
 		{"question": """A capital city, the name of movie starring Charlton Heston and the name of Jack Woltz's horse in the film The Godfather. Eight letters""", "answer": """Khartoum"""},
-
 		{"question": """New Vaudeville, Auguste, Character and Whiteface are all types of what?""", "answer": """Clowns"""},
-
 		{"question": """How is libel different from slander?""", "answer": """Libel is written"""},
-
 		{"question": """Which Russian word beginning with the letter B means great or large? (latin alphabet)""", "answer": """Bolshoi"""},
-
 		{"question": """What kind of storm does the arabic word 'HABOOB' describe?""", "answer": """Sandstorm"""},
-
 		{"question": """What is the English equivalent of the Welsh word "Popty Ping"?""", "answer": """Microwave"""},
-
 		{"question": """Blüthner, Erard, Chickering, Broadwood, Beckstein, Heintzman and Cristofori are all famous examples of what?""", "answer": """Pianos"""},
-
 		{"question": """German word meaning "spirit of the times" - nine letters""", "answer": """Zeitgeist"""},
-
 		{"question": """Scandinavian originating word meaning "a hard woven fiber formed into a rigid material" - six letters""", "answer": """Wicker"""},
-
 		{"question": """Swedish and Old Norse word for "commissary", "representative" and "steward" - nine letters, second letter 'M',""", "answer": """Ombudsman"""},
-
 		{"question": """The German word for "health" - ten letters""", "answer": """Gesundheit"""},
-
 		{"question": """Swedish word meaning "a meal served buffet-style" - eleven letters, second letter 'M', last letter 'D'.""", "answer": """Smorgasbord"""},
-
 		{"question": """Russian word meaning "way" or "journey" and literally translated is "travelling companion" - seven letters.""", "answer": """Sputnik"""},
-
 		{"question": """A meringue dessert named after a Russian ballet dancer.""", "answer": """Pavlova"""},
-
 		{"question": """The name for which kind of very elderly person stems from the Persian word for 'wax'?""", "answer": """Mummy"""},
-
 		{"question": """Which unpopular little word is Latin for poison or slimy liquid?""", "answer": """Virus"""},
-
 		{"question": """Shiguli and Zil are both examples of what in Russia?""", "answer": """Cars"""},
-
 		{"question": """The name for which vessel, often a habitat, stems from the Chinese for 'three' and 'plank'?""", "answer": """Sampan"""},
-
 		{"question": """A maritime term and the direct opposite of 'Tantrums'. One word.""", "answer": """Doldrums"""},
-
 		{"question": """The Pied Piper of Hamelin. What does 'pied' mean?""", "answer": """Multi-colored"""},
-
 		{"question": """What would 'Selenites' call home?""", "answer": """The Moon"""},
-
 		{"question": """Which misused latin word means '10% destroyed'?""", "answer": """Decimate"""},
-
 		{"question": """Which word meaning 'adroit' stems from the Latin word for 'right hand'?""", "answer": """Dexterity"""},
-
 		{"question": """Oromo, Igbo, Min Nan and Teluga are all examples of what?""", "answer": """Languages"""},
-
 		{"question": """From what language does the english word "Admiral" stems from?""", "answer": """Arabic """},
-
 		{"question": """I was a dead language for thousands of years, now i have 6 million native speakers, who am i?""", "answer": """Hebrew"""},
-
 		{"question": """Which Greek word for "grey" is also the name of a crippling childhood disease?""", "answer": """Polio"""},
-
 		{"question": """The meteorological term 'Southern Oscillation' is better known as what?""", "answer": """El Nino"""},
-
 		{"question": """In classical architecture, what are the two Greek architectural orders that end with the letter 'c'?(First word must begin with I and second with D)""", "answer": """Ionic and Doric"""},
-
 		{"question": """A modern problem, Nomophobia is the fear of what?""", "answer": """being without a mobile device"""},
-
 		{"question": """Which often used colloquialism for a certain political party stems from a 15th century Irish word for villainous robber?""", "answer": """Tory"""},
-
 		{"question": """From what language does the english word "Shampoo" stems from?""", "answer": """Hindi"""},
-
 		{"question": """Kar in Turkish, Lumi in Finnish, Neve in Italian, Nieve in Spanish and Neige in French . What is it called in English?""", "answer": """Snow"""},
-
 		{"question": """A magnificent palace, a variety of apple and a person or thing without equal. One word.""", "answer": """Nonsuch"""},
-
 		{"question": """A noun for chorus and a verb meaning to cease. Seven letters""", "answer": """Refrain"""},
-
 		{"question": """A black Perigord is an expensive example of what?""", "answer": """Truffle"""},
-
 		{"question": """What is the Greek word for fire?""", "answer": """Pyro"""},
-
 		{"question": """The rule of thumb or principle telling us that the simplest explanation is more likely the correct one is known as Occam's what?""", "answer": """Occam's Razor"""},
-
 		{"question": """Mariposa, Motyl, Farfalla, Borboleta, Papillon and Sommerfugl are all European words for what?""", "answer": """Butterfly"""},
-
 		{"question": """Which well known Sanskrit word can mean 'to unite' or 'to control'?""", "answer": """Yoga"""},
-
 		{"question": """The heir apparent to the French Throne between 1350 and 1791 was given which title?""", "answer": """The Dauphin of France"""},
-
 		{"question": """From what language does the english word "Ghetto" stems from?""", "answer": """Italian """},
-
 		{"question": """The British call it paraffin, what do the Americans call it?""", "answer": """Kerosene"""},
-
 		{"question": """Which French phrase, in common English use, means something already accomplished and considered irreversible?""", "answer": """Fait Accompli"""},
-
 		{"question": """Whose motto is 'my word is my bond'?""", "answer": """Stock Exchange"""},
-
 		{"question": """Which newspaper is nicknamed 'The Thunderer'?""", "answer": """The Times"""},
-
 		{"question": """What is the communications code word for the letter W?""", "answer": """Whiskey"""},
-
 		{"question": """What is the main use of the Java computer language?""", "answer": """Internet applications"""},
-
 		{"question": """How many in a Baker's dozen?""", "answer": """13"""},
-
 		{"question": """The words Plonk, Fiz, Burp, Woof, Oink, Meow,Roar and Chirp are?""", "answer": """Onomatopoeia"""},
-
 		{"question": """Reverse a portion to make an ambush?""", "answer": """Trap"""},
-
 		{"question": """Which famous pig is worth 1 dollar?""", "answer": """Guinea"""},
-
 		{"question": """What 8 letter word can be made from the letters found in the following sets of words Ale, Lake and link? (eg soon, sun and toil would be solution)""", "answer": """Alkaline"""},
-
 		{"question": """What does a Entomologist studies?""", "answer": """insects"""},
-
 		{"question": """the words Rotor, Radar, Hannah are examples of a...?""", "answer": """Palindrome"""},
-
-		{"question": """"The Archers" is a famous Brittish radio opera, what city in it's story shares almost the same name except for the letter C as a real UK city?""", "answer": """Ambridge"""}
+		{"question": """"The Archers" is a famous Brittish radio opera, what city in it's story shares almost the same name except for the letter C as a real UK city?""", "answer": """Ambridge"""},
+		{"question": "The japanese word for bread is パン (PAN), it has origin in what language?", "answer": "Portuguese"},
+		{"question": """The word "Bokeh" has its origins in what language?""", "answer": "Japanese"},
+		{"question": "The art of tending miniature trees is called?", "answer": "Bonsai"},
+		{"question": "A japanese word that literally means \"Empty Orchestra\"", "answer": "Karaoke"},
+		{"question": "The native religion of Japan", "answer": "Shinto"},
+		{"question": "A word that when translated literally means \"The Ocean Teacher\", a sort of spiritual leader in asia.", "answer": "Dalai Lama"},
+		{"question": """Word that when translated from it's native "Nahuatl" language literally means "Shaped like a testicle".""", "answer": "Avocado"},
+		{"question": """An english word from the czech for "forced labor", 5 letters.""", "answer": "Robot"},
+		{"question": """An english word originally used to refer to a fanatical muslim sect during the crusades, literally means "those who use hashish".""", "answer": "Assassin"},
+		{"question": """An english word from the Greek for "to tear flesh".""", "answer": "Sarcasm"},
+		{"question": """English word originating in the Greek for "Unfavorable conditions on the stars and planetary movements.".""", "answer": "Disaster"},
+		{"question": """English word from the Old french for "Death pledge" a kind of contractual payment.""", "answer": "Mortgage"},
+		{"question": """A disease that has its name from the italian for "Bad Air".""", "answer": "Malaria"},
+		{"question": "An english word that has its name thanks to one of Rome's famous 'Seven Hills', now meaning the official residence of a sovereign or important person.", "answer": "Palace"},
+		{"question": """The name of a very famous forest, originally meaning "Without Breasts".""", "answer": "Amazon"},
+		{"question": "What starts with the letter P and ends with the letter E, but it more than a million letters?", "answer": "Post Office"},
+		{"question": "If 2 is company and 3 is a crowd, what is 4 and 5?", "answer": "9"},
+		{"question": "This word has 3 letters, but if you take its last letter away it has four left.", "answer": "Ivy"},
+		{"question": "Which word has the most letters in it?", "answer": "Mailbox"},
+		{"question": "On a summer day it's currenly 7 o'clock, what time is it at the same time in ancient rome?", "answer": "1"},
+		{"question": "In what country is the pirahã language spoken?", "answer": "Brazil"},
+		{"question": "Where is waldo?", "answer": "<@785528225371521034>"},
+		{"question": """English word from the german for "corn weevil", a pet.""", "answer": "Hamster"},
+		{"question": "German, not working, broken.", "answer": "Kaput"},
+		{"question": "German, theme, often used in literature", "answer": "leitmotif "},
+		{"question": "Greek, Terrible lizard!", "answer": "Dinosaur"},
+		{"question": "Greek origin, To walk on the edge. Profession.", "answer": "Acrobat"},
+		{"question": """Combining the words "σχίζειν" — “to split” and "φρην" — “mind” you get this english word's etymology.""", "answer": "Schizophrenia"},
+		{"question": "An american state,snowy in spanish.", "answer": "Nevada"},
+		{"question": """"buckaroo" is anglicization of what spanish word?""", "answer": "Vaquero"},
+		{"question": """This english word comes from a misheard pronounciation of Spanish "el lagarto" - ‘the lizard'""", "answer": "Alligator"},
+		{"question": """Anglicization of the spanish word "cucaracha".""", "answer": "Cockroach"},
+		{"question": """Food, literally translated as “little donkey”.""", "answer": "Burrito"},
+		{"question": """Food, literally translated as "strained pineapple".""", "answer": "piña colada"},
+		{"question": "Animal, Old English: from slow + -th.", "answer": "Sloth"},
+		{"question": "Word from Italian 'cartone', a motion picture using animation techniques to photograph a sequence of drawings rather than real people or objects.", "answer": "Cartoon"},
+		{"question": """Word from Dutch koekje ‘little cake’""", "answer": "Cookie"}
 	]
 }
