bd05537d5df2416a9dd47083783aadd83856bb95
yagomichalak <yagomichalak@gmail.com>
2021-09-26 17:33:42 -0300

    FIXED inline checks

--- cogs/games.py
+++ cogs/games.py
@@ -319,9 +319,8 @@ class Games(commands.Cog):
 		if not self.active:
 			return await ctx.send(f"**{author.mention}, I'm not even playing yet!**")
 
-		perms = ctx.channel.permissions_for(author)
 		if self.multiplayer_active:
-			if await utils.is_allowed([mod_role_id]) or await utils.is_allowed_members([287746936587419648]):
+			if await utils.is_allowed([mod_role_id]).predicate(ctx) or await utils.is_allowed_members([287746936587419648]).predicate(ctx):
 				await self.reset_bot_status()
 				guild = ctx.message.guild
 				voice_client: discord.VoiceClient = discord.utils.get(self.client.voice_clients, guild=guild)
@@ -333,7 +332,7 @@ class Games(commands.Cog):
 				return await ctx.send("**Multiplayer session ended!**")
 			return await ctx.send(f"**{author.mention}, you cannot end a multiplayer session just like that!**")
 
-		if self.member.id == author.id or await utils.is_allowed([mod_role_id]) or await utils.is_allowed_members([287746936587419648]):
+		if self.member_id == author.id or await utils.is_allowed([mod_role_id]).predicate(ctx) or await utils.is_allowed_members([287746936587419648]).predicate(ctx):
 			await self.reset_bot_status()
 			guild = ctx.message.guild
 			voice_client: discord.VoiceClient = discord.utils.get(self.client.voice_clients, guild=guild)
